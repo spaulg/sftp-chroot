751454f314e261bc3884be073c6e232e64e20800
Simon Paulger <spaulger@codezen.co.uk>
2013-08-17 11:23:23 +0100

    git ignore

--- inbound_packet_parser.c
+++
@@ -1,5 +0,0 @@
-#include "header/inbound_packet_parser.h"
-
-int rewrite_inbound_packet(sftp_packet *packet) {
-    return 1;
-}
--- outbound_packet_parser.c
+++
@@ -1,5 +0,0 @@
-#include "header/outbound_packet_parser.h"
-
-int rewrite_outbound_packet(sftp_packet *packet) {
-    return 1;
-}
